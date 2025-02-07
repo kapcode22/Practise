e5a60e21fb5d2efc3904d705b3d5e6a534f1be5f
Mahesh Pawar <https://github.com/mpawar1234>
2025-02-08 00:13:45 +0530

    done

---
+++ frontend/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div>
+      <h1>this is header</h1>
+      <p>its content Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde excepturi odit qui fugiat consequatur ipsa rerum. At temporibus eveniet mollitia quo in impedit neque natus incidunt? Dolores unde nisi sit.</p>
+    </div>
+  )
+}
+
+export default header
